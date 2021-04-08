9f8cfd15da5330ba4c1670c22a48eca9b68ace33
Cam <cjordan@oakland.edu>
2021-04-08 14:49:37 -0400

    Initial Database Framework

---
+++ SQL/members.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2021 at 08:48 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `resolution-holdings`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `MEMBER_ID` int(11) NOT NULL COMMENT 'Auto Incrementing member ID.',
+  `FIRST_NAME` tinytext NOT NULL COMMENT 'The first name of the member.',
+  `LAST_NAME` tinytext NOT NULL COMMENT 'The last name of the member.',
+  `MIDDLE_INITIAL` tinytext NOT NULL COMMENT 'The middle initial of the member if applicable.',
+  `DOB` date NOT NULL COMMENT 'The date of birth of the member.',
+  `MEMBER_SINCE` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'When the member''s account was created.',
+  `GENDER` enum('M','F','O') NOT NULL COMMENT 'The member''s gender.',
+  `PHONE_NUMBER` tinytext DEFAULT NULL COMMENT 'The member''s phone number.',
+  `ADDRESS` text NOT NULL COMMENT 'The member''s home address.',
+  `EMAIL` tinytext DEFAULT NULL COMMENT 'The member''s email.',
+  `USERNAME` tinytext NOT NULL COMMENT 'The member''s username.',
+  `PASSWORD` tinytext NOT NULL COMMENT 'The member''s password. Hashed and salted for security purposes.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`MEMBER_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `MEMBER_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Auto Incrementing member ID.';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
