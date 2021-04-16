416a149982361a9f56641045f28f9bf5ac32596a
Cam <cjordan@oakland.edu>
2021-04-16 00:07:27 -0400

    Add files via upload

---
+++ transaction-info.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require('Includes/metadata.php'); ?>
+
+        <title>Resolution Holdings - Home</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+        <!-- Styles -->
+        <link href="style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php require('Includes/header.php'); ?>
+
+        <section>
+            <div class="centered container warning">
+                <h1>Warning!</h1>
+                <p>Please <a href="login.php">login</a> to view your account</p>
+            </div>
+        </section>
+    </body>
+</html>
