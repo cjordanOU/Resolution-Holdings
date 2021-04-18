07178bd7bb42e71bbd342c3ad5e060add71c1eb2
Cam <cjordan@oakland.edu>
2021-04-18 18:33:07 -0400

    Add files via upload

--- Includes/logout.php
+++ Includes/logout.php
@@ -5,6 +5,6 @@ $_SESSION = array();      // Clear all session variables
 session_destroy();        // Destroy the current session
 
 // Returns to the homepage
-header("location: index.php");
+header("location: ../index.php");
 exit;
 ?>
