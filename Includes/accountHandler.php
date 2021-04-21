ab2532e6d5872618fa510509b3d380201e23df46
Cam <cjordan@oakland.edu>
2021-04-21 00:43:06 -0400

    added separate logic for open/closed accounts

--- Includes/accountHandler.php
+++ Includes/accountHandler.php
@@ -1,5 +1,4 @@
 <?php
-    require_once 'Includes/loginCheck.php';
     // dbConfig include
     require_once 'Includes/dbConfig.php';
 
@@ -13,11 +12,24 @@
             //echo "<section class='account-box'><h2>Member ID: ". $_SESSION["id"] ."</h2>";
             echo "<section class='account-box'><h2>Account ID: ". $row["ACCOUNT_ID"] ."</h2>";
             echo "<div class='account-info'><h4>Account Type: ". ucfirst($row["ACCOUNT_TYPE"]) ."</h4>";
-            echo "<h4>Account Status: ". ucfirst($row["ACCOUNT_STATUS"]) ."</h4>";
-            echo "<h4>Account Created On: ". $row["CREATED"] ."</h4>";
-            echo "</div><div class='account-balance'>";
-            echo "<h3>Account Balance: &#36;". number_format($row["ACCOUNT_BALACE"],2) ."</h3>"; // Note: The database uses 'BALACE' not 'BALANCE'
-            echo "<a>View Transactions</a><br><a>Make Transaction</a>";
+            
+            
+            if (($row["ACCOUNT_STATUS"]) == 'open') {
+                echo "<h4>Account Status: <span class='account-open'>". ucfirst($row["ACCOUNT_STATUS"]) ."</span></h4>";
+                echo "<h4>Account Created On:". $row["CREATED"] ."</h4>";
+                echo "</div><div class='account-balance'>";
+                echo "<h3>Account Balance: &#36;". number_format($row["ACCOUNT_BALACE"],2) ."</h3>"; // Note: The database uses 'BALACE' not 'BALANCE'
+                echo "<a class='transaction-button' href='transaction-info.php' title='View the transactions associated with this account'>View Transactions</a><br><br><a class='transaction-button' href='transaction-create.php' title='Make a new transaction online'>Make Transaction</a>";
+            }
+            else {
+                echo "<h4>Account Status: <span class='account-shut'>". ucfirst($row["ACCOUNT_STATUS"]) ."</span></h4>";
+                echo "<h4>Account Created On:". $row["CREATED"] ."</h4>";
+                echo "</div><div class='account-balance'>";
+                echo "<h3>Account Balance: &#36;". number_format($row["ACCOUNT_BALACE"],2) ."</h3>"; // Note: The database uses 'BALACE' not 'BALANCE'
+                echo "<a class='transaction-button' href='transaction-info.php' title='View the transactions associated with this account'>View Transactions</a><br>";
+                echo "<p>Note: You cannot make new transactions with a closed or frozen account</p>";
+            }
+
             echo "</div></section>";
         }
         
