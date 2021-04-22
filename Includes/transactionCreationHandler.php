bb4f5c3597198464e4967478730b979eebf20777
Ingot <moneybagsx@yahoo.com>
2021-04-22 16:01:08 -0400

    Update transactionCreationHandler.php

--- Includes/transactionCreationHandler.php
+++ Includes/transactionCreationHandler.php
@@ -2,6 +2,7 @@
     // dbConfig include
     require_once 'Includes/dbConfig.php';
 
+    echo "Testing"
     // Define/Initialize variables
     $trnAccount = $_POST["transaction_account"];
     $trnAmount = "";
@@ -9,24 +10,27 @@
     $trnDesc = "";
 
     // Processing form data when form is submitted
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        echo "Testing2"
 
         $trnAmount = trim($_POST["transactionAmount"]);
         $trnType = trim($_POST["transactionType"]);
         $trnDesc = trim($_POST["transactionDescription"]);
 
         // Prepare an insert statement
-        $sql = "INSERT INTO transactions (AMOUNT, TYPE, DESCRIPTION, ACCOUNT_ID) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO transactions (`AMOUNT`, `TYPE`, `DESCRIPTION`, `ACCOUNT_ID`) VALUES (?, ?, ?, ?)";
 
         if($stmt = mysqli_prepare($connection, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ssss", $trnAmount, $trnType, $trnDesc, $trnAccount);
-
+            echo "Testing3"
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
+                echo "Testing4"
                 header("location: accounts.php");
-            } else{
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
 
