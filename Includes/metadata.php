6343c939767d8fce329bf43ef43adc6028ec2921
cjordanOU <cjordan@oakland.edu>
2021-04-23 19:30:20 -0400

    metadata.php now complies with php standard

--- Includes/metadata.php
+++ Includes/metadata.php
@@ -1,5 +1,7 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-<meta name="description" content="Insert Website Description Here.">
-<meta name="keywords" content="Keyword1, Keyword2, Keyword3, etc">
-<meta name="author" content="Author 1, Author 2, Author 3">
+<?php
+    echo '<meta charset="utf-8">';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    echo '<meta name="description" content="Insert Website Description Here.">';
+    echo '<meta name="keywords" content="Keyword1, Keyword2, Keyword3, etc">';
+    echo '<meta name="author" content="Author 1, Author 2, Author 3">';
+?>
