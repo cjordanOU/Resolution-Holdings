84c8ce0f5bd0285b509b71f32c8ce876ec022ca6
Cam <cjordan@oakland.edu>
2021-04-25 08:05:08 -0400

    Update metadata.php

--- Includes/metadata.php
+++ Includes/metadata.php
@@ -1,7 +1,7 @@
 <?php
     echo '<meta charset="utf-8">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-    echo '<meta name="description" content="Insert Website Description Here.">';
-    echo '<meta name="keywords" content="Keyword1, Keyword2, Keyword3, etc">';
-    echo '<meta name="author" content="Author 1, Author 2, Author 3">';
+    echo '<meta name="description" content="Website for Resolution Holdings Bank">';
+    echo '<meta name="keywords" content="Bank, Database, CSI3450">';
+    echo '<meta name="author" content="Cameron Jordan, Brandon Sharp, William Buerkle">';
 ?>
